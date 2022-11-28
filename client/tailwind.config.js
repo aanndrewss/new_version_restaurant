36d3ab1b353135941fa239a69bf8b96af614a4db
andrxw66 <andreyshihalev099@gmail.com>
2022-11-28 18:57:19 +0300

    refactoring scss, add tailwind config variables

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+const primaryColor = '#50FF40'
+const secondColor = '#089000'
+const bgColor = '#fff'
+
 module.exports = {
   content: [
     './pages/**/*.{js, ts, jsx, tsx}',
     './components/**/*.{js, ts, jsx, tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: primaryColor,
+        secondary: secondColor,
+        bgColor
+      },
+      borderRadius: {
+        layout: '1rem'
+      }
+    }
   },
   plugins: [
     require('tailwindcss'),
